7657383a8172cd5a8ee097c30e720254b577523d
jp-cordeiro <jpc.araujo@hotmail.com>
2020-05-28 22:53:23 -0300

    Updates

---
+++ precache-manifest.c7144b002f9c5b615cc508afe9c649de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fdd6451a6d840c38c041e04ef17073",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "5e93125906f1468627a2",
+    "url": "/mosters-rolodex/static/css/main.a6bcb90b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mosters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e93125906f1468627a2",
+    "url": "/mosters-rolodex/static/js/main.945600ac.chunk.js"
+  },
+  {
+    "revision": "3b1d11c5c06fc5abea79",
+    "url": "/mosters-rolodex/static/js/runtime-main.f637eaeb.js"
+  }
+]);
