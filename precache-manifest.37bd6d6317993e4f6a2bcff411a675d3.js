e7d119e06407d19e0f4dfd3a2d764422199035a9
jp-cordeiro <jpc.araujo@hotmail.com>
2020-05-28 22:33:11 -0300

    Updates

---
+++ precache-manifest.37bd6d6317993e4f6a2bcff411a675d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d6e10dd80923737004ee05b2ff12f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b33e635cff6313a6de9",
+    "url": "/static/css/main.a6bcb90b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b33e635cff6313a6de9",
+    "url": "/static/js/main.1d14d095.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
