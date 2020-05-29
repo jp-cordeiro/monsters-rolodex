86d1932f2261c5d7cb0440cb9ce37f5e0c845923
jp-cordeiro <jpc.araujo@hotmail.com>
2020-05-28 23:14:32 -0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -8,58 +8,7 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          "id": 1,
-          "name": "Leanne Graham",
-          "username": "Bret"
-        },
-        {
-          "id": 2,
-          "name": "Ervin Howell",
-          "username": "Antonette"          
-        },
-        {
-          "id": 3,
-          "name": "Clementine Bauch",
-          "username": "Samantha"
-        },
-        {
-          "id": 4,
-          "name": "Patricia Lebsack",
-          "username": "Karianne"
-        },
-        {
-          "id": 5,
-          "name": "Chelsey Dietrich",
-          "username": "Kamren"
-        },
-        {
-          "id": 6,
-          "name": "Mrs. Dennis Schulist",
-          "username": "Leopoldo_Corkery"
-        },
-        {
-          "id": 7,
-          "name": "Kurtis Weissnat",
-          "username": "Elwyn.Skiles"
-        },
-        {
-          "id": 8,
-          "name": "Nicholas Runolfsdottir V",
-          "username": "Maxime_Nienow"
-        },
-        {
-          "id": 9,
-          "name": "Glenna Reichert",
-          "username": "Delphine",
-        },
-        {
-          "id": 10,
-          "name": "Clementina DuBuque",
-          "username": "Moriah.Stanton"
-        }
-      ]
+      monsters: []
     };
   }
 
